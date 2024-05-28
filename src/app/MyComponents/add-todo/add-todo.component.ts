36f6f13fc76593da8b0c749372e5cf4f04134e00
Abhishek <abhishelke297127@gmail.com>
2024-05-28 11:58:36 +0530

    single todo will update here you can update 2nd as possible

--- src/app/MyComponents/add-todo/add-todo.component.ts
+++ src/app/MyComponents/add-todo/add-todo.component.ts
@@ -1,16 +1,17 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { Todo } from '../../Todo';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-todo',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-todo.component.html',
   styleUrl: './add-todo.component.css'
 })
 export class AddTodoComponent {
-  title!:string;
-  desc!:string;
+  title:string="";
+  desc:string="";
   @Output() todoAdd: EventEmitter<Todo>= new EventEmitter();
   onSubmit(){
     const todo={
